b8aefdee1399b51e389ec35310601046190df2e5
AnyRob8 <anyrob888@gmail.com>
2021-08-05 00:57:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import ERC20Abi from './ERC20Upgradeable.json'
+import { contract } from '@pooltogether/etherplex'
 
 function App() {
+  contract('POOL', ERC20Abi, '0x0cEC1A9154Ff802e7934Fc916Ed7Ca50bDE6844e')
   return (
     <div className="App">
       <header className="App-header">
